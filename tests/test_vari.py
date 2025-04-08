029a01657edb93f97c61de4644794ca6c1319d24
EnzoDeAngelis <vincenzodeangelis93@gmail.com>
2025-04-08 17:41:44 +0200

    Qualche esempio di test che dobbiamo integrare

--- tests/test_vari.py
+++ tests/test_vari.py
@@ -1,6 +1,20 @@
 import pytest
+from problems.orb2d_problem import swfun
+import numpy as np
 
 # NOTA - Il file può essere rinominato, ma deve iniziare come test_*.py
 
 def test_simple():
     assert 1 == 1
+    
+def test_swfun():
+    fullstate = np.ones(10)
+    data = [1, 2000, 5]
+    SF, LV = swfun(fullstate, data)
+    assert SF
+    assert LV
+    assert LV >= 0, 'Il primer vector deve essere positivo'
+    fullstate[4] = 0
+    with pytest.raises(ZeroDivisionError):
+        swfun(fullstate, data)
+    
