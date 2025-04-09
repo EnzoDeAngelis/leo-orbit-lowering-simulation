a5dd50267733af4bd6b50bb7b3739ee2b40b317b
EnzoDeAngelis <vincenzodeangelis93@gmail.com>
2025-04-09 23:26:03 +0200

    Aggiungo le funzioni swfun e compute_H per il 3d

--- OCP/2BP.py
+++ OCP/2BP.py
@@ -1,10 +1,18 @@
 from math import sin, cos, tan
+import numpy as np
+ 
 
-mu = 398600 
+def swfun(fullstate, data):
+    r, th, ph, u, v, w, m, lr, lth, lph, lu, lv, lw, lm = fullstate
+    T_max, u_e, _ = data
+    
+    LV = np.sqrt(lu**2 + lv**2 + lw**2)
+    return LV - lm*m/u_e, LV
 
 def ode_orb3d(t, fullstate, data, TS=None):
     r, th, ph, u, v, w, m, lr, lth, lph, lu, lv, lw, lm = fullstate
     T_max, u_e, _ = data
+    mu = 398600
     
     # switching function
     sf, LV = swfun(fullstate, data)
@@ -38,10 +46,41 @@ def ode_orb3d(t, fullstate, data, TS=None):
     # Costates
     dfdt[7] = (1/r**2) * (lth*v/cos(ph) + lph*w + lu*(-2*mu/r + v**2 + w**2) + lv*(-u*v + v*w*tan(ph)) + lw*(-u*w - v**2*tan(ph))) + ...
     dfdt[8] = ...
-    dfdt[9] = 1/r*cos(phi)**2*(-lth*v*sin(ph) - lv*v*w + lw*v**2) + ...
+    dfdt[9] = 1/r*cos(ph)**2*(-lth*v*sin(ph) - lv*v*w + lw*v**2) + ...
     dfdt[10] = (1/r) * (-lr*r + lv*v + lw*w)
     dfdt[11] = (1/r) * (-lth/cos(ph) - 2*lu*v + lv*(u - w*tan(ph)) + 2*lw*v*tan(ph))
     dfdt[12] = (1/r) * (-lph - 2*lu*w - lv*v*tan(ph) + lw*u)
     dfdt[13] = (T*LV) / m**2 + ...
     
     return dfdt
+
+def compute_H_orb3d(fullstate, data, TS=None):
+    r, th, ph, u, v, w, m, lr, lth, lph, lu, lv, lw, lm = fullstate
+    T_max, u_e, _ = data
+    mu = 398600
+    
+    sf, LV = swfun(fullstate, data)
+    
+    if TS is None:
+        T = T_max if sf > 0 else 0.0
+    else:
+        T = T_max if TS == 1 else 0.0
+        
+    au, av, aw = 0.0, 0.0, 0.0
+    adu, adv, adw = 0.0, 0.0, 0.0
+    if (T > 0.0) and (LV > 1e-12):
+        au = (T/m) * (lu / LV) + adu
+        av = (T/m) * (lv / LV) + adv
+        aw = (T/m) * (lw / LV) + adw
+    else:
+        au = adu
+        av = adv
+        aw = adw
+        
+    return(
+        lr*u + lth*v/(r*cos(ph)) + lph*w/r + 
+        lu*(-mu/r**2 + v**2/r + w**2/r + au) + 
+        lv*(-u*v/r + v*(w/r)*tan(ph) + av) + 
+        lw*(-u*w/r - (v**2/r)*tan(ph) + aw)
+        - lm*T/u_e
+    )
